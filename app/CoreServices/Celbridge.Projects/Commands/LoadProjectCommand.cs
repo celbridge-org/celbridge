d5a65fb16a609a66d06cddc4cfe32559c41470fc
amazinggitboy <matt@planetmatty.com>
2025-09-30 17:22:31 +0100

    Adding PersistentPage, and driving Community and Workspace Pages from it
    - Adding Levels of Persistence : Eternal and Project, to allow control over how persistent pages might be
    - Adding Creation of Default Readme.md file in new projects
    - Fix for new project behaviour with Persistent Pages
    - Some style fixes.

--- app/CoreServices/Celbridge.Projects/Commands/LoadProjectCommand.cs
+++ app/CoreServices/Celbridge.Projects/Commands/LoadProjectCommand.cs
@@ -1,10 +1,12 @@
 using Celbridge.Commands;
+using Celbridge.Documents;
 using Celbridge.Dialog;
-using Celbridge.Messaging;
 using Celbridge.Navigation;
 using Celbridge.Settings;
 using Celbridge.Workspace;
 using Microsoft.Extensions.Localization;
+
+using Path = System.IO.Path;
 
 namespace Celbridge.Projects.Commands;
 
@@ -13,7 +15,6 @@ public class LoadProjectCommand : CommandBase, ILoadProjectCommand
     private const string HomePageName = "HomePage";
     private const string WorkspacePageName = "WorkspacePage";
 
-    private readonly IMessengerService _messengerService;
     private readonly ICommandService _commandService;
     private readonly IWorkspaceWrapper _workspaceWrapper;
     private readonly IProjectService _projectService;
@@ -23,7 +24,6 @@ public class LoadProjectCommand : CommandBase, ILoadProjectCommand
     private readonly IStringLocalizer _stringLocalizer;
 
     public LoadProjectCommand(
-        IMessengerService messengerService,
         IStringLocalizer stringLocalizer,
         ICommandService commandService,
         IProjectService projectService,
@@ -32,7 +32,6 @@ public class LoadProjectCommand : CommandBase, ILoadProjectCommand
         IDialogService dialogService,
         IWorkspaceWrapper workspaceWrapper)
     {
-        _messengerService = messengerService;
         _stringLocalizer = stringLocalizer;
         _commandService = commandService;
         _projectService = projectService;
@@ -58,12 +57,8 @@ public class LoadProjectCommand : CommandBase, ILoadProjectCommand
             return Result.Ok();
         }
 
-        // Change the Navigation Cache status of the Workspace Page to Disabled, to allow it to be destroyed.
-        if (_workspaceWrapper.IsWorkspacePageLoaded)
-        {
-            var message = new SetWorkspacePagePersistenceMessage(false);
-            _messengerService.Send(message);
-        }
+        // Change the Navigation Cache status of the active persistent pages to Disabled, to allow them to be destroyed.
+        _navigationService.ClearPersistenceOfAllLoadedPages();
 
         // Close any loaded project.
         // This will fail if there's no project currently open, but we can just ignore that.
@@ -89,6 +84,46 @@ public class LoadProjectCommand : CommandBase, ILoadProjectCommand
         }
 
         _editorSettings.PreviousProject = ProjectFilePath;
+
+        // Opening Welcome Markdown document on opening of project if the file exists and is accessible.
+        var targetFilePath = new ResourceKey("readme.md");
+
+        var resourceRegistry = _workspaceWrapper.WorkspaceService.ExplorerService.ResourceRegistry;
+
+        // %%% Currently we have a foible, where having differing cases in the filenames from the expected can make the case pass
+        //  the spot checks for the file being available on Windows, but then fail to load as the Loading command seems to be case sensitive.
+        var filePath = resourceRegistry.GetResourcePath(targetFilePath);
+        if (!string.IsNullOrEmpty(filePath) &&
+            File.Exists(filePath))
+        {
+            try
+            {
+                // Ensure the file is accessible.
+                //  This would be done better using DocumentsService.CanAccessFile but DocumentsService isn't created until
+                //  the explorer starts and we may be reaching here before then.
+                var fileInfo = new FileInfo(filePath);
+                using var stream = fileInfo.Open(FileMode.Open, FileAccess.Read, FileShare.Read);
+
+                // Execute a command to open the HTML document.
+                _commandService.Execute<IOpenDocumentCommand>(command =>
+                {
+                    command.FileResource = targetFilePath;
+                    command.ForceReload = false;
+                });
+
+                // Execute a command to select the welcome document
+                _commandService.Execute<ISelectDocumentCommand>(command =>
+                {
+                    command.FileResource = new ResourceKey(targetFilePath);
+                });
+            }
+            catch (IOException)
+            {
+            }
+            catch (UnauthorizedAccessException)
+            {
+            }
+        }
 
         return Result.Ok();
     }
