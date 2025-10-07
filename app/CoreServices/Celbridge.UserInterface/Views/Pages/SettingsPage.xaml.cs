608f5d17f7ca67087f124df8916da50d308bd1cd
amazinggitboy <matt@planetmatty.com>
2025-10-07 14:45:19 +0100

    Some tidies on the recent Settings page work.

--- app/CoreServices/Celbridge.UserInterface/Views/Pages/SettingsPage.xaml.cs
+++ app/CoreServices/Celbridge.UserInterface/Views/Pages/SettingsPage.xaml.cs
@@ -1,11 +1,4 @@
-using Celbridge.Projects;
-using Celbridge.UserInterface.Models;
-using Celbridge.UserInterface.ViewModels.Pages;
-using System.Runtime.CompilerServices;
-using Celbridge.Entities;
 using Celbridge.Settings;
-using Celbridge.UserInterface;
-using Microsoft.Extensions.Localization;
 
 namespace Celbridge.UserInterface.Views;
 
@@ -14,11 +7,13 @@ namespace Celbridge.UserInterface.Views;
 /// </summary>
 public sealed partial class SettingsPage : PersistentPage
 {
-    Dictionary<ApplicationColorTheme, string> ThemeToNameLookupDictionary = new();
+    private readonly Dictionary<ApplicationColorTheme, string> _themeToNameLookupDictionary = new();
 
     private readonly IEditorSettings _editorSettings;
     private readonly IStringLocalizer _stringLocalizer;
     private readonly IUserInterfaceService _userInterfaceService;
+    public LocalizedString TitleString => _stringLocalizer.GetString($"SettingsPage_Title");
+    public LocalizedString ApplicationThemeString => _stringLocalizer.GetString($"SettingsPage_ApplicationTheme");
 
     public SettingsPage()
     {
@@ -30,13 +25,13 @@ public sealed partial class SettingsPage : PersistentPage
         var ThemeValues = Enum.GetValues(typeof(ApplicationColorTheme));
         foreach (ApplicationColorTheme themeEntry in ThemeValues)
         {
-            string name = $"Theme_" + Enum.GetName(typeof(ApplicationColorTheme), themeEntry);
+            var name = $"Theme_" + Enum.GetName(typeof(ApplicationColorTheme), themeEntry);
             var localizedName = _stringLocalizer.GetString(name);
             if (localizedName == null)
             {
                 throw new NotImplementedException("Cannot find localised string entry for '" + name + "'");
             }
-            ThemeToNameLookupDictionary.Add(themeEntry, localizedName);
+            _themeToNameLookupDictionary.Add(themeEntry, localizedName);
         }
 
         Persistence = PersistenceLevel.Eternal;
@@ -44,11 +39,10 @@ public sealed partial class SettingsPage : PersistentPage
         this.InitializeComponent();
 
         // Set up our Theme options and default selection.
-        ApplicationThemeComboBox.ItemsSource = ThemeToNameLookupDictionary;
+        ApplicationThemeComboBox.ItemsSource = _themeToNameLookupDictionary;
         ApplicationThemeComboBox.DisplayMemberPath = "Value";
         ApplicationThemeComboBox.SelectedValuePath = "Key";
 
-        // %%% Need to Unload these.
         ApplicationThemeComboBox.Loaded += ApplicationThemeComboBox_Loaded;
         ApplicationThemeComboBox.SelectionChanged += ApplicationThemeComboBox_SelectionChanged;
     }
@@ -74,7 +68,7 @@ public sealed partial class SettingsPage : PersistentPage
         // Wasn't able to set the selection directly with the values using SelectedItem due to instancing complications making
         //  it simpler to do it this way, unfortunately.
         int index = 0;
-        foreach (KeyValuePair<ApplicationColorTheme, string> themeEntry in ThemeToNameLookupDictionary)
+        foreach (KeyValuePair<ApplicationColorTheme, string> themeEntry in _themeToNameLookupDictionary)
         {
             if (themeEntry.Key == _editorSettings.Theme)
             {
