1237001975bada62efb7339c7068ea5a84a73dfb
amazinggitboy <matt@planetmatty.com>
2025-10-09 11:40:04 +0100

    Hiding Explorer button until project is loaded, and hiding Create Project button for invalid paths
    The Create New Project Dialog now by default offers the previous used path if it is present and valid, and otherwise offers the Users default document home path, if this is valid. We also now check for changes in the folder path and hide the Create Button if these invalidate the folder path.
    Fixes issue #422

--- app/CoreServices/Celbridge.UserInterface/ViewModels/Dialogs/NewProjectDialogViewModel.cs
+++ app/CoreServices/Celbridge.UserInterface/ViewModels/Dialogs/NewProjectDialogViewModel.cs
@@ -45,7 +45,14 @@ public partial class NewProjectDialogViewModel : ObservableObject
         _projectService = projectService;
         _filePickerService = filePickerService;
 
-        _destFolderPath = _editorSettings.PreviousNewProjectFolderPath;
+        // Set default path for projects. If there is no previous project folder, or it doesn't exist, then use our users documents home folder.
+        //  NOTE : It is possible for the home documents folder to become invalid if the user has messed up their windows set up, but in that case we only really have the choice
+        //          of offering the path they have or a potentially empty previous path. As the Create button is hidden for invalid paths then it is safe for us to allow this.
+        //             We could potentially start listing the drives and offering the root folder of drives but then this could fail due to permissions, so I think this is probably enough.
+        _destFolderPath = (_editorSettings.PreviousNewProjectFolderPath == string.Empty || !Directory.Exists(_editorSettings.PreviousNewProjectFolderPath))
+                            && Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments))
+                                ? Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) // + $"\\Celbridge" 
+                                    : _editorSettings.PreviousNewProjectFolderPath;
 
         PropertyChanged += NewProjectDialogViewModel_PropertyChanged;
     }
@@ -67,6 +74,14 @@ public partial class NewProjectDialogViewModel : ObservableObject
             if (!ResourceKey.IsValidSegment(ProjectName))
             {
                 // Project name is not a valid filename
+                IsCreateButtonEnabled = false;
+                DestProjectFilePath = string.Empty;
+                return;
+            }
+
+            if (!Directory.Exists(DestFolderPath))
+            {
+                // Project base folder is not valid.
                 IsCreateButtonEnabled = false;
                 DestProjectFilePath = string.Empty;
                 return;
