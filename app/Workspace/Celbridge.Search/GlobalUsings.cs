4059bd1f9e8753e1f2f247ce75b8a8a42ab0309e
Chris Gregan <chrisgregan@gmail.com>
2026-01-28 16:56:41 +0000

    Remove unnecessary usings

--- app/Workspace/Celbridge.Search/GlobalUsings.cs
+++ app/Workspace/Celbridge.Search/GlobalUsings.cs
@@ -1,9 +1,1 @@
-global using System;
-global using System.Collections.Generic;
-global using System.Threading.Tasks;
-
-global using CommunityToolkit.Mvvm.Input;
-
-global using Microsoft.UI.Xaml;
-global using Microsoft.UI.Xaml.Controls;
-global using Microsoft.UI.Xaml.Input;
+global using Celbridge.Core;
