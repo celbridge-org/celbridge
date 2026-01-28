6a25e0fffe80e37e23991133bcce180de130a96e
Chris Gregan <chrisgregan@gmail.com>
2026-01-28 16:27:50 +0000

    Add AddResourceHelper to DI

--- app/Workspace/Celbridge.Resources/ServiceConfiguration.cs
+++ app/Workspace/Celbridge.Resources/ServiceConfiguration.cs
@@ -20,6 +20,7 @@ public static class ServiceConfiguration
         services.AddTransient<IResourceRegistryDumper, ResourceRegistryDumper>();
         services.AddTransient<IResourceNameValidator, ResourceNameValidator>();
         services.AddTransient<IResourceMonitor, ResourceMonitor>();
+        services.AddTransient<AddResourceHelper>();
 
         //
         // Register commands
