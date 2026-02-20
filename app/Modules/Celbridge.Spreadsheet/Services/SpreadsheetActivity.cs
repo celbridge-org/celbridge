f3bb41e7d6526aa09ec820c1fe92f338bc13139d
Christopher Gregan <chrisgregan@gmail.com>
2026-02-20 08:23:24 +0000

    Move spreadsheet save monitoring to activity class (#634)

--- app/Modules/Celbridge.Spreadsheet/Services/SpreadsheetActivity.cs
+++ app/Modules/Celbridge.Spreadsheet/Services/SpreadsheetActivity.cs
@@ -1,33 +1,48 @@
 using Celbridge.Activities;
 using Celbridge.Commands;
+using Celbridge.Console;
+using Celbridge.Documents;
 using Celbridge.Entities;
 using Celbridge.Explorer;
+using Celbridge.Logging;
+using Celbridge.Messaging;
 using Celbridge.Screenplay.Components;
 using Celbridge.Workspace;
+using System.Text.Json;
 
 namespace Celbridge.Spreadsheet.Services;
 
 public class SpreadsheetActivity : IActivity
 {
+    private readonly ILogger<SpreadsheetActivity> _logger;
+    private readonly IMessengerService _messengerService;
     private readonly ICommandService _commandService;
     private readonly IEntityService _entityService;
 
     public SpreadsheetActivity(
+        ILogger<SpreadsheetActivity> logger,
+        IMessengerService messengerService,
         ICommandService commandService,
         IWorkspaceWrapper workspaceWrapper)
     {
+        _logger = logger;
+        _messengerService = messengerService;
         _commandService = commandService;
         _entityService = workspaceWrapper.WorkspaceService.EntityService;
     }
 
     public async Task<Result> ActivateAsync()
     {
+        _messengerService.Register<DocumentSaveCompletedMessage>(this, OnDocumentSaveCompletedMessage);
+
         await Task.CompletedTask;
         return Result.Ok();
     }
 
     public async Task<Result> DeactivateAsync()
     {
+        _messengerService.Unregister<DocumentSaveCompletedMessage>(this);
+
         await Task.CompletedTask;
         return Result.Ok();
     }
@@ -139,5 +154,44 @@ public class SpreadsheetActivity : IActivity
         await Task.CompletedTask;
 
         return Result.Ok();
+    }
+
+    private void OnDocumentSaveCompletedMessage(object recipient, DocumentSaveCompletedMessage message)
+    {
+        var savedResource = message.DocumentResource;
+        if (!SupportsResource(savedResource))
+        {
+            return;
+        }
+
+        var getComponentResult = _entityService.GetComponentOfType(savedResource, SpreadsheetEditor.ComponentType);
+        if (getComponentResult.IsFailure)
+        {
+            return;
+        }
+        var component = getComponentResult.Value;
+
+        var getResult = component.GetProperty("/runOnDataChange");
+        if (getResult.IsFailure)
+        {
+            return;
+        }
+
+        var runOnDataChange = JsonSerializer.Deserialize<bool>(getResult.Value);
+        if (!runOnDataChange)
+        {
+            return;
+        }
+
+        var scriptResource = component.GetString("/pythonScript");
+        var arguments = component.GetString("/arguments");
+
+        _logger.LogInformation($"Running script: {scriptResource} with arguments: {arguments}");
+
+        _commandService.Execute<IRunCommand>(command =>
+        {
+            command.ScriptResource = scriptResource;
+            command.Arguments = arguments;
+        });
     }
 }
